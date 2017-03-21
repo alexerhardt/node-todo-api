5874e7c7b3daebeb20d9312d57245e92b4083418
Alex <alex.erhardt.es@gmail.com>
2017-03-21 16:13:41 +0000

    set up separate DB test

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
-var _ = require('lodash');
+require('./config/config');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo.js');
